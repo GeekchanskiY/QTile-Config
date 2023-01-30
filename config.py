bcff3f4e2a7370ef613167b29ccffd8122b900ad
GeekchanskiY <astrovskiy.04@gmail.com>
2023-01-30 16:31:33 +0300

    add volume widget, gitignore

--- config.py
+++ config.py
@@ -10,24 +10,41 @@
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+
+
 #
 #       GeekanskiY's QTile Config
 #
 
-from libqtile import bar, layout, widget
+from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
+from widgets.volume.volume_widget import VolumeStatus, increace_volume
+
+import os
+import subprocess
+
+
 mod = "mod4"
-terminal = guess_terminal()
+terminal = "kitty"
+browser = "google-chrome"
+
+second_monitor = True
+
+@hook.subscribe.startup
+def autostart():
+    home=os.path.expanduser('~/.config/qtile/autostart.sh')
+    subprocess.Popen(home)
+
 
 keys = [
 
-    Key([mod], "w",
-        lazy.to_screen(0)),
-    Key([mod], "e",
-        lazy.to_screen(1)),
+    Key([mod, 'shift'], "w",
+       lazy.to_screen(0)),
+    Key([mod, 'shift'], "q",
+      lazy.to_screen(1)),
 
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
@@ -36,7 +53,7 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    #Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
@@ -69,7 +86,44 @@ keys = [
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
 
-groups = [Group(i) for i in "123456789"]
+
+#
+#   Widget settings and shortcuts
+#
+
+# Keyboard
+
+keyboard = widget.KeyboardLayout(configured_keyboards=['us', 'ru'])
+keys.extend([
+    Key([mod,], "space", lazy.widget["keyboardlayout"].next_keyboard(),   desc="Next keyboard layout"),
+ ])
+
+# Volume Mixer
+
+volume = VolumeStatus()
+
+@lazy.function
+def increace_volume(qtile):
+    volume.plus_volume()
+
+@lazy.function
+def decrease_volume(qtile):
+    volume.minus_volume()
+    
+
+keys.extend([
+    Key([mod], "f", increace_volume, desc="Decrease_volume"),
+    Key([mod], "d", decrease_volume, desc="Add volume"),
+])
+
+
+
+
+
+
+
+
+groups = [Group(i) for i in "123456"]
 
 for i in groups:
     keys.extend(
@@ -96,10 +150,10 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
+    layout.Columns(border_focus="#fff", border_width=2, margin=2), # type: ignore
+    # layout.Max(), # type: ignore
     # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
+    # layout.Stack(num_stacks=3),
     # layout.Bsp(),
     # layout.Matrix(),
     # layout.MonadTall(),
@@ -120,9 +174,13 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        wallpaper="~/Pictures/a.png",
+        wallpaper_mode='fill',
+        top=bar.Bar(
             [
-                widget.CurrentLayout(),
+                keyboard,
+                volume,
+                #widget.CurrentLayout(),
                 widget.GroupBox(),
                 widget.Prompt(),
                 widget.WindowName(),
@@ -132,12 +190,12 @@ screens = [
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                # widget.TextBox("GeekanskiY's config", name="default"),
+                # widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
-                widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                # widget.Systray(),
+                widget.Clock(format="%H:%M"),
                 widget.QuickExit(),
             ],
             24,
@@ -145,12 +203,6 @@ screens = [
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
     ),
-    Screen(
-        bottom=bar.Bar([
-            widget.GroupBox(),
-            widget.WindowName(),
-            ], 30),
-        )
 ]
 
 # Drag floating layouts.
@@ -167,7 +219,6 @@ bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(
     float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
@@ -181,19 +232,24 @@ auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
 
-# When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 wmname = "LG3D"
+
+
+if second_monitor:
+    screens.append(
+        Screen(
+        wallpaper="~/Pictures/a.png",
+        wallpaper_mode='fill',
+
+        bottom=bar.Bar([
+            widget.GroupBox(),
+            widget.WindowName(),
+        ], 24),
+        )
+
+    )
+
